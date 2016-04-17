e0f3ecabe79088016182ab3f0c27c1b0d5a4c88e
ankitsharmaks <ankitsharmaks@gmail.com>
2016-04-17 09:26:21 -0700

    working indexer

--- public/hod/preProcessor.js
+++ public/hod/preProcessor.js
@@ -8,16 +8,17 @@ var addindex = require('./add_to_text_index');
 */
 console.log(json_helper);
 
-/*for (var i = tokenList.length - 1; i >= 0; i--) {
+for (var i = tokenList.length - 1; i >= 0; i--) {
 	var token = tokenList[i];
-	console.log("Fetching data for token "+token);*/
-	var token ="devesh";
-	crawler.crawlNYTimes(token, "20150412", "20150812", function callback(response) {
+	console.log("Fetching data for token "+token);
+	//var token ="elon musk";
+	crawler.crawlNYTimes(token, "20150412", "20150413", function callback(response) {
 		//console.log(response);
 		console.log("Data from nytimes fetched for token "+token);
 		var hodJson = json_helper.convertNewsJsonHodIndexJson(JSON.parse(response));
+
 		setTimeout(function(){
-						addindex.callAddToIndexAPI(hodJson);
+						addindex.callAddToIndexAPI(JSON.stringify(hodJson));
 		}, 2000);
 	});
-/*};*/
+};
