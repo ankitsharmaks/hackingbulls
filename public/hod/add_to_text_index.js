2dcf8e2337b28681365ccfa0c3d341167399b90a
Subhranil Banerjee <subhrani@buffalo.edu>
2016-04-17 12:22:43 -0700

    Avoid some merge conflicts.

--- public/hod/add_to_text_index.js
+++ public/hod/add_to_text_index.js
@@ -1,17 +1,17 @@
 var havenondemand = require('havenondemand');
 //var jsonfile = require('./file.json');
-var client = new havenondemand.HODClient('a18b21db-7694-45a3-8741-6c955b545969', 'v1');
+var client = new havenondemand.HODClient('5799741a-d1cf-4d31-bccd-e67bbd335700', 'v1');
 
 //callAddToIndexAPI(JSON.stringify(jsonfile));
 
 exports.callAddToIndexAPI = function callAddToIndexAPI(jsonObj, delegateCallback) {
-	var data = {"json":""+jsonObj,
-				"index":'angel'};
-	/*data = {};
+	//var data = {json:"{\"document\":[ {\"url\":\"http://www.yahoo.com\",\"snippet\":\"some snippet test\",\"source\":\"some source\", \"title\":\"some long title about this index\"}]}",
+				//index:'test'};
+	data = {};
 	data.json = jsonObj;
-	data.index = "hackingbulls"*/
+	data.index = "hackingbulls"
 
-	console.log(JSON.stringify(data))
+	console.log(jsonObj)
 	console.log("Started call add to index API")
 	client.call('addtotextindex', data, function(err, resp, body){
 		console.log("addtotextindex callback body:");
